dcaa5223f70f0b5f14ae8797407a8beefd6c966f
mahnueldev <emmanuelgbd3@gmail.com>
2023-08-09 15:36:57 +0100

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function About() {
+    // throw new Error('Not implemented');
+  return (
+    <>
+    <h1> About</h1>
+    <Link href="/">Link to home page</Link>
+    </>
+  )
+}
